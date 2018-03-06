72e002c0a61496ed4e3d8a58ab831af5dd4edd7c
billgrayqGH <bgquitalig@yahoo.com>
2018-03-05 21:12:47 -0500

    Spring Boot Angular Name Lister
    CRUD application in Spring Boot and Angular 4

---
+++ src/main/java/com/example/demo/CrudBackendApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+@SpringBootApplication
+public class CrudBackendApplication implements CommandLineRunner{
+	@Autowired
+	private UserRepository userRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		userRepository.save(new User("Hello","World"));
+		userRepository.save(new User("Test","Foo"));
+		userRepository.save(new User("John","Doe"));
+	}
+}
